2e0597e155be1b0e5dd1fca96f4f6da15e1450f8
Adam Jeffreson <adam.jeffreson@gmail.com>
2023-02-06 13:54:26 +0000

    added view componentes for nav bars for recepie and stock tabs

---
+++ resources/views/layouts/stock_navigation.blade.php
@@ -0,0 +1,22 @@
+<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-6">
+    <div class="flex justify-between h-16 mb-4">
+        <div class="flex mt-3 mb-4 ">
+            <!-- Logo -->
+            <!-- Navigation Links -->
+            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <x-nav-link :href="route('welcome')" :active="request()->routeIs('welcome')">
+                    {{ __('All Stock') }}
+                </x-nav-link>
+                <x-nav-link :href="route('recepies')" :active="request()->routeIs('recepies')">
+                    {{ __('Add Stock') }}
+                </x-nav-link>
+                <x-nav-link :href="route('stock')" :active="request()->routeIs('stock')">
+                    {{ __('Stock Info') }}
+                </x-nav-link>
+                <x-nav-link :href="route('stock')" :active="request()->routeIs('stock')">
+                    {{ __('Other') }}
+                </x-nav-link>
+            </div>
+        </div>
+    </div>
+</div>
