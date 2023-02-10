4648142b6d3998f0a2fe0315f06ff890b8870de7
Adam Jeffreson <adam.jeffreson@gmail.com>
2023-02-10 12:33:41 +0000

    bug in way stock was displayed

--- resources/views/stock.blade.php
+++ resources/views/stock.blade.php
@@ -11,7 +11,6 @@
             <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div>
-                        {{$stocks}}
                         <table>
                             <tr>
                                 <th>Stock ID</th>
