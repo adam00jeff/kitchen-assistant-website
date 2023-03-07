98b3ff0fe01b4c8cac2616b27998d075d7c0e194
Adam Jeffreson <adam.jeffreson@gmail.com>
2023-03-07 11:13:36 +0000

    changed case of BusinessSeeder.php

--- database/seeders/BusinessSeeder.php
+++ database/seeders/BusinessSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\business;
+use App\Models\Business;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
