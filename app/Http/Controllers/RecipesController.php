83844b7b89b8c486e34a6f947e22b78dadc10ff7
Adam Jeffreson <adam.jeffreson@gmail.com>
2023-03-15 15:54:11 +0000

    going to try and yolo all the way to most recent commit that had huge bug

--- app/Http/Controllers/RecipesController.php
+++ app/Http/Controllers/RecipesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Recipe;
+use App\Models\Stock;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -17,20 +18,31 @@ class RecipesController extends Controller
 
     public function create_recipe()
     {
-        return view('recipes-form');
+        $stocks = Stock::all();
+        return view('recipes-form',['stocks' => $stocks]);
     }
     public function store_recipe(Request $request)
     {        $busid = Auth::user()->business_id;
         $id = Auth::id();
         $validated = $request->validate([
             'name' => 'required|max:255',
-            'ingredients' => 'required|max:255',//supplier ID for now, to be replaced with plain text entry
+            /*'ingredients' => 'required|max:255',//supplier ID for now, to be replaced with plain text entry*/
             'rmethod'=>'required|max:1000'
         ]);
 
+        $req = $request->addMoreIngredients;
+        $qty = $request->quantity;
+        /*ddd($qty);*/
+        $i=1;
+        foreach ($req as $k=>$v){
+            foreach ($v as $k2 => $v2) {
+                $req[$k][$k2] = $v2." ".$qty[$i];
+            }
+        $i++;
+    }
         $recipe = new Recipe;
         $recipe->name = $request->name;
-        $recipe->ingredients = $request->ingredients;
+        $recipe->ingredients = $req;
         $recipe->method = $request->rmethod;
         $recipe->user_id = $id;
         $recipe->business_id = $busid;
--- resources/views/recipes-form.blade.php
+++ resources/views/recipes-form.blade.php
@@ -22,20 +22,28 @@
             <form method="POST" action="/recipes" class="" enctype="multipart/form-data">
                 @csrf
                 <div class="">
+                    {{--Name--}}
                     <p class="text-gray-700 text-sm">
-                        <input
-                            class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
-                            id="name" name="name" type="text" placeholder="recipe name">
+                        <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
+                               id="name" name="name" type="text" placeholder="recipe name">
                     </p>
-                    <p class="text-gray-700 mt-2 text-sm">
-                        <input
-                            class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
-                            id="ingredients" name="ingredients" type="text" placeholder="ingredients">
-                    </p>
+
+                    <table  id="dynamicAddRemoveIngredient" class="p-2">
+                        <tr>
+                        </tr>
+                    </table>
+                    <button type="button" name="add" id="dynamic-ar-ingredient" class="bg-gray-800 px-2 py-2 text-white text-xs rounded-md uppercase hover:underline">Add Ingredient from Stock</button>
+
                     <p class="text-gray-500 text-base mt-2">
-                        <input
-                            class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
-                            id="rmethod" name="rmethod" type="text" placeholder="method">
+                        <label>Method</label>
+
+                    <table  id="dynamicAddRemoveStep" class="p-2">
+                        <tr>
+                            <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
+                                   id="rmethod" name="rmethod" type="text" placeholder="Step 1">
+                        </tr>
+                    </table>
+                    <button type="button" name="addStep" id="dynamic-ar-step" class="bg-gray-800 px-2 py-2 text-white text-xs rounded-md uppercase hover:underline">Add Another Step</button>
                     </p>
 
 
@@ -49,4 +57,41 @@
             </form>
         </div>
     </div>
+
+    <!-- JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript">
+        let i = 0;
+        let step = 2;
+        let stepcount=2;
+        let deletecount=0;
+        let newstep =0;
+        $("#dynamic-ar-ingredient").click(function () {
+            ++i;
+            $("#dynamicAddRemoveIngredient").append(
+                '<tr><td><select name="addMoreIngredients['+i+'][stock]" id="stock['+1+']" class="form-control col-md-12" required>@foreach($stocks as $stock)<option value="{{ $stock['name']}}">{{ $stock['name']}}</option>@endforeach</select></td><td><input class="block bg-grey-lighter text-grey-darker border rounded"id="quantity['+i+']" name="quantity['+i+']" type="text" placeholder="quantity ({{$stock['serving_unit']}})"></td><td></td><td><button type="button" class="remove-input-field">Delete</button></td></tr><br>');
+        });
+        $("#dynamic-ar-step").click(function () {
+            $("#dynamicAddRemoveStep").append(
+                '<tr><td><input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" id="rmethod" name="rmethod" type="text" placeholder="Step '+step+'"></td><td></td><td><button type="button" class="remove-step-field">Delete</button></td></tr><br>');
+            ++step;
+            ++stepcount;
+        });
+        $(document).on('click', '.remove-step-field', function () {
+            $(this).parents('tr').remove();
+            ++deletecount;
+            newstep = stepcount-deletecount;
+            if (step >= newstep){
+                newstep++;
+            }
+            step = newstep;
+        });
+
+        $(document).on('click', '.remove-input-field', function () {
+            $(this).parents('tr').remove();
+        });
+    </script>
 </x-app-layout>
+
+
