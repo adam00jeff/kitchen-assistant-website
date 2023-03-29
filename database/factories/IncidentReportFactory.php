5d18a8cc74eafa6c0cbe800ba20cb48f0b78afcf
Adam Jeffreson <adam.jeffreson@gmail.com>
2023-03-29 13:04:22 +0100

    can now seed incident reports

--- database/factories/IncidentReportFactory.php
+++ database/factories/IncidentReportFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\IncidentReport>
@@ -17,7 +18,17 @@ class IncidentReportFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'date_of_report' => $this->faker->dateTimeBetween('-1 month'),
+            'date_of_incident' => $this->faker->dateTimeBetween('-2 month'),
+            'time_of_incident' => $this->faker->time(),
+            'incident_type'=>$this->faker->randomElement(['Customer Incident','Staff Incident','Food Incident','Stock/ Supplier Incident','Workplace Incident','Other']),
+            'severity'=>$this->faker->randomElement(['High','Medium','Low']),
+            'location'=>$this->faker->randomElement(['Front of House','Back of House','Outside Building']),
+            'description'=>$this->faker->sentences(4,true),
+            'action_taken'=>$this->faker->sentences(4,true),
+            'reported_by'=>'test',
+            'user_id'=> rand(1,5),
+            'business_id'=> rand(1,5)
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,8 @@ use App\Models\Recipe;
 use App\Models\Stock;
 use App\Models\supplier;
 use App\Models\User;
+use App\Models\IncidentReport;
+use Database\Factories\IncidentReportFactory;
 use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
@@ -27,16 +29,14 @@ class DatabaseSeeder extends Seeder
             ['id'=>3],
             ['id'=>4],
             ['id'=>5],
-            ['id'=>6],
-
-        ))->create();
+            ['id'=>6],))->create();
         $this->call(UserSeeder::class);
         User::factory(30)->state(new Sequence(
             ['business_id'=>2],
             ['business_id'=>3],
             ['business_id'=>4],
             ['business_id'=>5],
-            ['business_id'=>6]       ))->create();
+            ['business_id'=>6]))->create();
         $this->call(NutrientSeeder::class);
         $this->call(StockSeeder::class);
         $this->call(RecipeSeeder::class);
@@ -44,49 +44,14 @@ class DatabaseSeeder extends Seeder
         $this->call(ContactSeeder::class);
         $this->call(AllergenSeeder::class);
         $this->call(SupplierSeeder::class);
-
-
-
-
-
-
-/*        Stock::factory(50)->state(new Sequence(
-            ['user_id'=>1],
-            ['user_id'=>2],
-            ['user_id'=>3],
-            ['user_id'=>4],
-            ['user_id'=>5]))->state(new Sequence(
-            ['image'=>"https://nix-tag-images.s3.amazonaws.com/159_thumb.jpg"],
-            ['image'=>"https://nix-tag-images.s3.amazonaws.com/653_thumb.jpg"],
-            ['image'=>"https://nix-tag-images.s3.amazonaws.com/834_thumb.jpg"],
-            ['image'=>"https://nix-tag-images.s3.amazonaws.com/115_thumb.jpg"],
-            ['image'=>"https://nix-tag-images.s3.amazonaws.com/320_thumb.jpg"]
-        ))->create();*/
+        $this->call(IncidentReportSeeder::class);
+        IncidentReport::factory(25)->create();
         Recipe::factory(50)->state(new Sequence(
             ['user_id'=>1],
             ['user_id'=>2],
             ['user_id'=>3],
             ['user_id'=>4],
-            ['user_id'=>5],
-
-        ))->create();
-/*        Document::factory(50)->state(new Sequence(
-            ['renewal_period'=>'annual'],
-            ['renewal_period'=>'quarterly']
-        ))->state(new Sequence(
-            ['user_id'=>1],
-            ['user_id'=>2],
-            ['user_id'=>3],
-            ['user_id'=>4],
-            ['user_id'=>5],
-
-        ))->create();*/
+            ['user_id'=>5],))->create();
         Supplier::factory(10)->create();
-
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
